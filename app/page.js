01cc0840f66a3cd9579cc94d2c6fe4ef1568e95f
rishita0210 <rishitadixit02@gmail.com>
2024-06-05 01:08:53 +0530

    first commit

---
+++ app/page.js
@@ -0,0 +1,29 @@
+<<<<<<< HEAD
+
+export default function Home() {
+
+  return (
+    <div>
+      Subscribe to ZAINKEEPSCODE :)
+    </div>
+  );
+=======
+import Navbar from '@/src/utils/components/Navbar/Navbar'
+import Hero from '@/src/utils/components/Hero/Hero'
+import BrandingVideo from '@/src/utils/components/BrandingVideo/BrandingVideo'
+import Whatwedo from '@/src/utils/components/Whatwedo/Whatwedo'
+import './page.css'
+
+export default function Home(){
+    return(
+        <div classname="app">
+            <Navbar/>
+            <Hero/>
+            <BrandingVideo/>
+            <Whatwedo/>
+          
+        </div>
+    );
+
+>>>>>>> origin/main
+}
